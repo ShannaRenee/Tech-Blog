59fe81b902f1facf08d92e74121e4de3b504d100
Calcifer <shanna.renee.morris@gmail.com>
2023-09-03 23:14:33 -0700

    created route for one user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const {User} = require('../../models')
+const sequelize = require('../../config/connection')
+const {User, Post, Comment} = require('../../models')
 
 
 router.post('/', async (req, res) => {
@@ -10,6 +11,62 @@ router.post('/', async (req, res) => {
             password: req.body.password
         })
         res.status(201).json(newUser)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.findAll({
+            attributes: {
+                exclude: ['password'],
+                include: [
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM post WHERE post.userId = user.id)'),
+                        'postsCount',
+                    ],
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM comment WHERE comment.userId = user.id)'),
+                        'commentsCount',
+                    ],
+                ],
+            },
+        });
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.get('/:userId', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.userId, {
+            include: [
+                { model: Post },
+                { model: Comment, include: { model: Post, attributes: 
+                ['title'] } }
+                ],
+            attributes: {
+                exclude: ['password'],
+                include: [
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM post WHERE post.userId = user.id)'),
+                        'postsCount',
+                    ],
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM comment WHERE comment.userId = user.id)'),
+                        'commentsCount',
+                    ],
+                ]
+            },
+        });
+
+        if (!user) return res.status(404).json({ message: 'No user foudnd.' });
+
+        res.status(200).json(user);
     } catch (error) {
         console.log(error);
         res.status(500).json(error)
