b470c405635bca2fe5cf001af42b3f7fbda38644
Calcifer <shanna.renee.morris@gmail.com>
2023-09-04 16:18:56 -0700

    preparing to deploy to heroku

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const sequelize = require('./config/connection');
 
 const routes = require('./controllers')
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 const sess = {
