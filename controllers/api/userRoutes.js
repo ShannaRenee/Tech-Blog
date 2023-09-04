8c278efbe25afb60c8e7f3f04c981b5702984690
Calcifer <shanna.renee.morris@gmail.com>
2023-09-04 15:49:00 -0700

    created log in and log out routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,13 +10,18 @@ router.post('/', async (req, res) => {
             email: req.body.email,
             password: req.body.password
         })
-        res.status(201).json(newUser)
+        req.session.save(() => {
+            (req.session.userId = newUser.id),
+            (req.session.loggedIn = true);
+            res.status(201).json(newUser);
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json(error)
     }
 });
 
+// Route to retrieve all users
 router.get('/', async (req, res) => {
     try {
         const users = await User.findAll({
@@ -41,6 +46,43 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+// Route to retrieve logged in user's profile
+router.get('/profile', async (req, res) => {
+    if (!req.session.loggedIn) return res.redirect('/login');
+    try {
+        const user = await User.findByPk(req.session.userId, {
+            include: [
+                { model: Post },
+                { model: Comment, include: { model: Post, attributes: 
+                ['title'] } }
+                ],
+            attributes: {
+                exclude: ['password'],
+                include: [
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM post WHERE post.userId = user.id)'),
+                        'postsCount',
+                    ],
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM comment WHERE comment.userId = user.id)'),
+                        'commentsCount',
+                    ],
+                ]
+            },
+        });
+
+        if (!user) return res.status(404).json({ message: 'No user foudnd.' });
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+});
+
+
+// Route to retrieve a single user by id
 router.get('/:userId', async (req, res) => {
     try {
         const user = await User.findByPk(req.params.userId, {
@@ -73,11 +115,11 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
-router.put('/:userId', async (req, res) => {
+router.put('/profile', async (req, res) => {
     try {
         const updatedUser = await User.update(req.body, {
             where: {
-                id: req.params.userId,
+                id: req.session.userId,
             },
             individualHooks: true,
         });
@@ -91,6 +133,27 @@ router.put('/:userId', async (req, res) => {
     }
 });
 
+// route to delete a user by id
+router.delete('/profile', async (req, res) => {
+    if (!req.session.loggedIn) return res.redirect('/login');
+    try {
+        const deletedUser = await User.destroy({
+            where: {
+                id: req.session.userId,
+            },
+        });
+
+        if (!deletedUser) return res.status(404).json({ message: 'No user found.' })
+
+        res.status(202).json(deletedUser)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
+
+
+// Admin can  delete user
 router.delete('/:userId', async (req, res) => {
     try {
         const deletedUser = await User.destroy({
@@ -107,6 +170,44 @@ router.delete('/:userId', async (req, res) => {
         console.log(error);
         res.status(500).json(error);
     }
+});
+
+// LOGIN ROUTE
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: { email: req.body.email }
+        });
+
+        if (!user) 
+            return res.status(400).json({message: 'credentials not valid.'});
+
+        const validPW = await user.checkPassword(req.body.password);
+        if (!validPW) 
+            return res.status(400).json({message: 'credentials not valid.'});
+
+        req.session.save(() => {
+            req.session.userId = user.id
+            req.session.loggedIn = true;
+            res.status(200).json(user)
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+});
+
+
+// LOGOUT ROUTE
+router.post('/logout', async (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
 })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const sequelize = require('./config/connection')
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
 
-require('./models');
 
 const routes = require('./controllers')
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
